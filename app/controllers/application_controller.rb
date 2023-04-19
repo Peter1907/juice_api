56158318f82b7771d07a1bbd24e7c1be754d7fbc
Peter1907 <peter.nady.1408@gmail.com>
2023-04-19 20:02:50 +0200

    Implemet jwt authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
   def response_hash(message, data)
     { message: message, data: data }
+  end
+
+  private
+
+  def decode
+    token = request.headers["Authorization"].split(" ").last
+    payload = JWT.decode(token, 'secret_juice')[0]
+    @current_user = User.find(payload['id'])
+  rescue
+    'Invalid token'
+  end
+
+  def authenticate_request
+    if decode == 'Invalid token'
+      render json: response_hash("error", "Invalid token"), status: :unauthorized
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
+      resources :users, only: [:create, :destroy]
       resources :articles, only: [:index, :show, :create, :update, :destroy]
+      post "/login", to: "sessions#login"
     end
   end
 end
